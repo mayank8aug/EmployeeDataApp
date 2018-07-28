7ec4fde68788e711ae6fe30b36d8904fe9a5cbdb
mayank8aug <mayank8aug@gmail.com>
2018-07-28 12:27:39 +0530

    removeEmployee API

--- app/queries.js
+++ app/queries.js
@@ -1,7 +1,7 @@
 var promise = require('bluebird');
 
 var options = {
-    promiseLib: promise
+  promiseLib: promise
 };
 
 var pgPromise = require('pg-promise')(options);
@@ -10,71 +10,85 @@ var database = pgPromise(connectionString);
 
 function getAllEmployees(req, res, next) {
   database.any('select * from employee')
-      .then(function (data) {
-        res.status(200)
-          .json({
-            status: 'success',
-            data: data,
-            message: 'Successfully fetched all employees'
-          });
-      })
-      .catch(function (err) {
-        return next(err);
-      });
-  }
-  
-  function addEmployee(req, res, next) {
-    database.none('insert into employee(ename, age, gender, sal, dept)' +
-        'values(${ename}, ${age}, ${gender}, ${sal}, ${dept})',
-      req.body)
-      .then(function () {
-        res.status(200)
-          .json({
-            status: 'success',
-            message: 'Added new employee'
-          });
-      })
-      .catch(function (err) {
-        return next(err);
-      });
-  }
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Successfully fetched all employees'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
 
-  function getEmployee(req, res, next) {
-    var empId = parseInt(req.params.id);
-    database.one('select * from employee where id = $1', empId)
-      .then(function (data) {
-        res.status(200)
-          .json({
-            status: 'success',
-            data: data,
-            message: 'Successfully fetched employee with id: ' + empId
-          });
-      })
-      .catch(function (err) {
-        return next(err);
-      });
-  }
+function addEmployee(req, res, next) {
+  database.none('insert into employee(ename, age, gender, sal, dept)' +
+    'values(${ename}, ${age}, ${gender}, ${sal}, ${dept})',
+    req.body)
+    .then(function () {
+      res.status(200)
+        .json({
+          status: 'success',
+          message: 'Added new employee'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
 
-  function updateEmployee(req, res, next) {
-    database.none('update employee set sal=$1 where id=$2',
-      [req.body.sal, parseInt(req.params.id)])
-      .then(function () {
-        res.status(200)
-          .json({
-            status: 'success',
-            message: 'Updated the employee successfully.' 
-          });
-      })
-      .catch(function (err) {
-        return next(err);
-      });
-  }
-  
+function getEmployee(req, res, next) {
+  var empId = parseInt(req.params.id);
+  database.one('select * from employee where id = $1', empId)
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Successfully fetched employee with id: ' + empId
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
 
-  module.exports = {
-    getAllEmployees: getAllEmployees,
-    addEmployee: addEmployee,
-    getEmployee: getEmployee,
-    updateEmployee: updateEmployee/*,
-    removeEmployee: removeEmployee */
+function updateEmployee(req, res, next) {
+  database.none('update employee set sal=$1 where id=$2',
+    [req.body.sal, parseInt(req.params.id)])
+    .then(function () {
+      res.status(200)
+        .json({
+          status: 'success',
+          message: 'Updated the employee successfully.'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+function removeEmployee(req, res, next) {
+  var empId = parseInt(req.params.id);
+  database.result('delete from employee where id = $1', empId)
+    .then(function (result) {
+      res.status(200)
+        .json({
+          status: 'success',
+          message: 'Removed employee successfully.'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+module.exports = {
+  getAllEmployees: getAllEmployees,
+  addEmployee: addEmployee,
+  getEmployee: getEmployee,
+  updateEmployee: updateEmployee,
+  removeEmployee: removeEmployee
 };
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,14 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
 var db = require('../queries');
-
 
 router.get('/api/employees', db.getAllEmployees);
 router.post('/api/employee', db.addEmployee);
 router.get('/api/employee/:id', db.getEmployee);
 router.put('/api/employee/:id', db.updateEmployee);
-/*router.delete('/api/employee/:id', db.removeEmployee); */
-
+router.delete('/api/employee/:id', db.removeEmployee);
 
 module.exports = router;
